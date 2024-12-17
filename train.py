24f9a2e1ac68c03df13777df2b39e697c262116a
Haruki Suto <j3344jou@sit.ac.jp>
2024-12-17 21:35:21 +0900

    修正

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-"""ニューラルネットワークの学習を行う"""
+
 import time
 
 import matplotlib.pyplot as plt
@@ -10,16 +10,13 @@ import torchvision.transforms.v2 as transforms
 import models
 
 
-# GPU があれば 'cuda' なければ 'cpu' というデバイス名を設定
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# データセットの前処理関数
 ds_transform = transforms.Compose([
     transforms.ToImage(),
     transforms.ToDtype(torch.float32, scale=True)
 ])
 
-# データセットの読み込み
 ds_train = datasets.FashionMNIST(
     root='data',
     train=True,
@@ -29,12 +26,11 @@ ds_train = datasets.FashionMNIST(
 
 ds_test = datasets.FashionMNIST(
     root='data',
-    train=False,  # テスト用データセット
+    train=False,  
     download=True,
     transform=ds_transform
 )
 
-# ミニバッチのデータローダー
 bs = 64
 dataloader_train = torch.utils.data.DataLoader(
     ds_train,
@@ -52,22 +48,18 @@ for image_batch, label_batch in dataloader_train:
     print(label_batch.shape)
     break
 
-# モデルのインスタンスを作成
 model = models.MyModel()
 
-# 精度を計算する
 acc_train = models.test_accuracy(model, dataloader_train, device=device)
 print(f'train accuracy: {acc_train*100:.3f}%')
 acc_test = models.test_accuracy(model, dataloader_test, device=device)
 print(f'test accuracy: {acc_test*100:.3f}%')
 
-# ロス関数の選択
 loss_fn = torch.nn.CrossEntropyLoss()
 
-# 最適化手法の選択
 learning_rate = 0.003
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-# criterion (規準) とも呼ぶ
+
 
 n_epochs = 20
 
@@ -79,7 +71,7 @@ acc_test_history = []
 for k in range(n_epochs):
     print(f'epoch {k+1}/{n_epochs}', end=': ', flush=True)
 
-    # 1 epoch の学習
+    
     time_start = time.time()
     loss_train = models.train(model, dataloader_train, loss_fn, optimizer, device=device)
     time_end = time.time()
@@ -92,7 +84,6 @@ for k in range(n_epochs):
     loss_test_history.append(loss_test)
     print(f'test loss: {loss_test:.3f} ({time_end-time_start:.1f}s)')
 
-    # 精度を計算する
     if (k+1) % 5 == 0:
         time_start = time.time()
         acc_train = models.test_accuracy(model, dataloader_train, device=device)
